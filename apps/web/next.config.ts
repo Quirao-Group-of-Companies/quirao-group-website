f9a03fe2c5d109ed5f1388fc90766131b02d6c54
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-18 15:21:49 +0800

    feat(web): Add 10mb limit upload to resume upload

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   transpilePackages: ['@repo/db'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
