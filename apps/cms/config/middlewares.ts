e8c0d0d2c15858984050ffce88fa76e5df841fa9
Kent-J-A <kentjasper.abella1@gmail.com>
2026-02-23 09:51:15 +0800

    Update middlewares.ts

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -6,9 +6,24 @@ export default [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'https://*.supabase.co', 'https://*.supabase.in'],
-            'media-src': ["'self'", 'data:', 'blob:', 'https://*.supabase.co', 'https://*.supabase.in'],
+          'connect-src': [
+            "'self'", 
+            'https:'
+          ],
+          'img-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'https://*.supabase.co', 
+            'https://*.supabase.in'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://*.supabase.co',
+            'https://*.supabase.in',
+          ],
           upgradeInsecureRequests: null,
         },
       },
