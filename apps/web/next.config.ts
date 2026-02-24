aad8bde75b99b8204c8fb6de0f34ab9f1c6592ae
Els <101701179+elsdave@users.noreply.github.com>
2026-02-24 14:04:03 +0800

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const supabaseHostname = process.env.SUPABASE_HOSTNAME || '';
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL?.replace(/^https?:\/\//, ''),
+        hostname: supabaseHostname,
         port: '',
-        pathname: '/storage/v1/object/public/**',
+        pathname: '/storage/v1/object/public/strapi-uploads/**',
       },
     ],
+  },
+  reactCompiler: true,
+  transpilePackages: ['@repo/db'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
   },
 };
 
