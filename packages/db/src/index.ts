455d8cc781e9184dd4082a1ddfac0f4a4030fb63
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-25 14:54:23 +0800

    chore: Apply linting changes

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,7 +5,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../drizzle/schema';
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('❌ DATABASE_URL is missing. Please check your .env file.');
+}
 
 const client = postgres(connectionString, { prepare: false });
 
