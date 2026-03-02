c247428b87dab53c540157b6826cdff0ee9454fb
Evren <99727483+DeybLims@users.noreply.github.com>
2026-03-02 15:13:56 +0800

    feat: update deploy.yml and plugins.ts for environment variable management

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,14 +1,9 @@
 import type { Core } from '@strapi/strapi';
 
-export default ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+export default (_: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
   upload: {
     config: {
-      provider: 'strapi-provider-upload-supabase',
-      providerOptions: {
-        apiUrl: env('NEXT_PUBLIC_SUPABASE_URL'),
-        apiKey: env('SUPABASE_SERVICE_ROLE_KEY'),
-        bucket: env('SUPABASE_BUCKET_NAME'),
-      },
+      provider: 'local',
     },
   },
 });
